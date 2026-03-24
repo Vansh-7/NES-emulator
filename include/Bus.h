2b1213efe2990f2433890f2e8952e3e393cb4cf0
Vansh Gupta <vanshgupta1490@gmail.com>
2026-03-24 13:22:08 +0530

    feat(bus): implement bus with 64KB RAM and read/write methods

---
+++ include/Bus.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <cstdint>
+#include <array>
+
+#include "cpu6502.h"
+
+class Bus {
+public:
+	Bus();
+	~Bus();
+
+    // Devices on bus
+	cpu6502 cpu;	
+
+	// Fake RAM
+	std::array<uint8_t, 64 * 1024> ram;
+
+    // Bus Read & Write
+	void write(uint16_t addr, uint8_t data);
+	uint8_t read(uint16_t addr, bool bReadOnly = false);
+};
