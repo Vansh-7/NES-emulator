26bce07dfaf9bb1946893d25b3b11688dc5f0eab
Vansh Gupta <vanshgupta1490@gmail.com>
2026-06-06 22:02:35 +0530

    refactor(cartridge): extend iNES loader to instantiate advanced mappers
    - Map iNES IDs 1, 2, 3, 4, and 66 to safely initialize respective concrete Mapper subclasses
    - Pass correct dynamic PRG/CHR bank configurations down during instantiation

--- source/Cartridge.cpp
+++ source/Cartridge.cpp
@@ -3,7 +3,8 @@
 Cartridge::Cartridge(const std::string& sFileName)
 {   
     // iNES Format Header
-    struct sHeader{
+    struct sHeader
+	{
         char name[4];
         uint8_t prg_rom_chunks;
 		uint8_t chr_rom_chunks;
@@ -30,10 +31,11 @@ Cartridge::Cartridge(const std::string& sFileName)
 
         // Determine Mapper ID
 		nMapperID = ((header.mapper2 >> 4) << 4) | (header.mapper1 >> 4);
-		mirror = (header.mapper1 & 0x01) ? VERTICAL : HORIZONTAL;
+		hw_mirror = (header.mapper1 & 0x01) ? VERTICAL : HORIZONTAL;
 
         // "Discover" File Format
 		uint8_t nFileType = 1;
+		if ((header.mapper2 & 0x0C) == 0x08) nFileType = 2;
 
 		if (nFileType == 0)
 		{
@@ -59,16 +61,24 @@ Cartridge::Cartridge(const std::string& sFileName)
 
 		if (nFileType == 2)
 		{
+			nPRGBanks = ((header.prg_ram_size & 0x07) << 8) | header.prg_rom_chunks;
+			vPRGMemory.resize(nPRGBanks * 16384);
+			ifs.read((char*)vPRGMemory.data(), vPRGMemory.size());
 
+			nCHRBanks = ((header.prg_ram_size & 0x38) << 8) | header.chr_rom_chunks;
+			vCHRMemory.resize(nCHRBanks * 8192);
+			ifs.read((char*)vCHRMemory.data(), vCHRMemory.size());
 		}
 
         // Load appropriate mapper
 		switch (nMapperID)
 		{
-		case 0: pMapper = std::make_shared<Mapper_000>(nPRGBanks, nCHRBanks); break;
-		//case   2: pMapper = std::make_shared<Mapper_002>(nPRGBanks, nCHRBanks); break;
-		//case   3: pMapper = std::make_shared<Mapper_003>(nPRGBanks, nCHRBanks); break;
-		//case  66: pMapper = std::make_shared<Mapper_066>(nPRGBanks, nCHRBanks); break;
+			case   0: pMapper = std::make_shared<Mapper_000>(nPRGBanks, nCHRBanks); break;
+			case   1: pMapper = std::make_shared<Mapper_001>(nPRGBanks, nCHRBanks); break;
+			case   2: pMapper = std::make_shared<Mapper_002>(nPRGBanks, nCHRBanks); break;
+			case   3: pMapper = std::make_shared<Mapper_003>(nPRGBanks, nCHRBanks); break;
+			case   4: pMapper = std::make_shared<Mapper_004>(nPRGBanks, nCHRBanks); break;
+			case  66: pMapper = std::make_shared<Mapper_066>(nPRGBanks, nCHRBanks); break;
 		}
 
 		bImageValid = true;
@@ -91,9 +101,15 @@ bool Cartridge::ImageValid()
 bool Cartridge::cpuRead(uint16_t addr, uint8_t &data)
 {
 	uint32_t mapped_addr = 0;
-	if (pMapper->cpuMapRead(addr, mapped_addr))
-	{
-		data = vPRGMemory[mapped_addr];
+	if (pMapper->cpuMapRead(addr, mapped_addr, data)) {
+		if (mapped_addr == 0xFFFFFFFF) {
+			// Mapper has actually set the data value, for example cartridge based RAM
+			return true;
+		}
+		else {
+			// Mapper has produced an offset into cartridge bank memory
+			data = vPRGMemory[mapped_addr];
+		}
 		return true;
 	}
 	else
@@ -103,9 +119,16 @@ bool Cartridge::cpuRead(uint16_t addr, uint8_t &data)
 bool Cartridge::cpuWrite(uint16_t addr, uint8_t data)
 {
 	uint32_t mapped_addr = 0;
-	if (pMapper->cpuMapWrite(addr, mapped_addr))
+	if (pMapper->cpuMapWrite(addr, mapped_addr, data))
 	{
-		vPRGMemory[mapped_addr] = data;
+		if (mapped_addr == 0xFFFFFFFF) {
+			// Mapper has actually set the data value, for example cartridge based RAM
+			return true;
+		}
+		else {
+			// Mapper has produced an offset into cartridge bank memory
+			vPRGMemory[mapped_addr] = data;
+		}
 		return true;
 	}
 	else
@@ -142,4 +165,24 @@ void Cartridge::reset()
 	// but does reset the mapper.
 	if (pMapper != nullptr)
 		pMapper->reset();
+}
+
+MIRROR Cartridge::Mirror()
+{
+	MIRROR m = pMapper->mirror();
+	if (m == MIRROR::HARDWARE) {
+		// Mirror configuration was defined
+		// in hardware via soldering
+		return hw_mirror;
+	}
+	else {
+		// Mirror configuration can be
+		// dynamically set via mapper
+		return m;
+	}
+}
+
+std::shared_ptr<Mapper> Cartridge::GetMapper()
+{
+	return pMapper;
 }
