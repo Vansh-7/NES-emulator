b1ddf27e712f0ee899b61987937828726ee5984b
Vansh Gupta <vanshgupta1490@gmail.com>
2026-06-07 10:59:25 +0530

    fix: resolve merge conflicts on main branch pull

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -137,7 +137,11 @@ private:
 	bool OnUserCreate() override
 	{
 		// Load the cartridge
+<<<<<<< Updated upstream
 		cart = std::make_shared<Cartridge>("ROM/DonkeyKong.nes");
+=======
+		cart = std::make_shared<Cartridge>("ROM/Zelda II - The Adventure of Link.nes");
+>>>>>>> Stashed changes
 		
 		if (!cart->ImageValid())
 			return false;
